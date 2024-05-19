a0bc72c485972a9fc665a9b5c0e14149c387d09d
Hein Min Latt <heinminlatt.ygn001@gmail.com>
2024-05-19 22:28:14 +0630

    Folder

--- App.jsx
+++ App.jsx
@@ -1,8 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React,{useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import Flatapi from './src/components/Flatapi';
 import Flatapione from './src/components/Flatapione';
-
 
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
     </View>
   );
 };
-
 export default App;
 
 const styles = StyleSheet.create({
